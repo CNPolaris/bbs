cd895647717186bc869464445195e8a243adae37
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2022-06-08 09:21:20 +0800

    修改评论的表结构

--- src/main/java/com/polaris/bbs/mapper/BbsReplyMapper.java
+++ src/main/java/com/polaris/bbs/mapper/BbsReplyMapper.java
@@ -9,7 +9,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * </p>
  *
  * @author polaris
- * @since 2022-05-30
+ * @since 2022-06-08
  */
 public interface BbsReplyMapper extends BaseMapper<BbsReply> {
 
